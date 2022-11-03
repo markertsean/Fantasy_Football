0505752ee2f12d80d8c2ebe35414b281a8cb1a1d
Sean Markert <markertsean0@gmail.com>
2022-11-03 11:20:45 -0700

    Added data structures file

---
+++ model/data_structures.py
@@ -0,0 +1,183 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+class ZScaler:
+    def __init__(self,inp_df,columns=None):
+        self.scale_dict = {}
+        assert isinstance(inp_df,pd.DataFrame) # Input must be a dataframe
+        if (columns is None):
+            self.add(inp_df)
+        else:
+            if (not isinstance(columns,list)):
+                columns = [columns]
+            self.add(inp_df[columns])
+
+    def __repr__(self):
+        out_str = ""
+        for key in self.scale_dict:
+            out_str += "\tField: {:50s}\tMean: {:10.6f}\tStd: {:10.6f}\n".format(
+                key,
+                self.scale_dict[key]['mean'],
+                self.scale_dict[key]['std'],
+            )
+        return out_str
+
+    def __str__(self):
+        return "Member of ZScaler\n"+self.__repr__()
+
+    def add(self,inp_df):
+        for col in inp_df.columns.values:
+            self.scale_dict[col] = {}
+            self.scale_dict[col]['mean'] = inp_df[col].mean()
+            self.scale_dict[col]['std'] = inp_df[col].std()
+
+    def remove(self,col):
+        del self.scale_dict[col]
+
+    def get_dict(self):
+        return self.scale_dict
+
+    def get(self,col,kind=None):
+        if (kind is None):
+            return self.scale_dict[col]
+        elif((kind=='mean') or (kind=='std')):
+            return self.scale_dict
+        else:
+            raise ValueError("kind must be 'mean' or 'std'")
+
+    def scale_cols(self,inp_df,cols):
+        out_df = inp_df.copy()
+        for col in cols:
+            assert col in self.scale_dict # Can only scale columns in the scaler!
+            out_df[col]=(out_df[col]-self.scale_dict[col]['mean'])/self.scale_dict[col]['std']
+            out_df.rename(columns={col:col+'_z'},inplace=True)
+        return out_df
+
+#TODO: move w/ ^
+class PCACols:
+    def __init__(self,inp_df,columns,n_components):
+        self.columns = columns
+        self.n_components = n_components
+        self.model_pca = PCA(n_components=n_components)
+        self.model_pca.fit(inp_df[self.columns])
+
+    def __repr__(self):
+        out_str = ""
+        out_str+= "\tN components = "+str(self.n_components)+"\n"
+        out_str+= "\tColumns = [\n"
+        for col in self.columns:
+            out_str+="\t\t"+str(col)+",\n"
+        out_str+= "\t]\n"
+        out_str+= "\tCumulative Explained Variance = [\n"
+        for var in self.model_pca.explained_variance_ratio_.cumsum():
+            out_str+="\t\t"+str(var)+",\n"
+        out_str+= "\t]\n"
+        return out_str
+
+    def __str__(self):
+        return "Member of PLCACols\n"+self.__repr__()
+
+    def PCA(self):
+        return self.model_pca
+
+    def transform(self,inp_df):
+        return self.model_pca.transform(inp_df[self.columns])
+
+class ModelWrapper:
+    def __init__(
+        self,
+        input_x_df,
+        input_y_df,
+        key_fields=['season','week','team','opponent']
+    ):
+        self.x_df = input_x_df.dropna()
+        self.y_df = input_y_df.loc[
+            input_y_df.index.intersection(self.x_df.index)]
+        ].dropna()
+        
+        assert self.x_df.shape[0]==self.y_df.shape[0] # X and Y must have same dimension
+        self.key_fields = key_fields
+        self.model_dict = {}
+        self.col_dict = {}
+        self.cv_dict = {}
+
+    def __get_values__(self,input_df,cols):
+        return input_df.drop(columns=self.key_fields)[cols].values
+
+    def get_model_dict(self):
+        return self.model_dict
+
+    def get_model_predicted_fields(self):
+        return self.col_dict.keys()
+
+    def get_model_predictor_fields(self):
+        return self.col_dict
+
+    def get_cv_dict(self):
+        return self.cv_dict
+
+    def model(self,name):
+        assert name in self.model_dict
+        return self.model_dict[name]
+
+    def train_model(
+        self,
+        names,
+        model,
+        parameters=None,
+        use_cols=None,
+        test_size=0.20,
+        n_jobs=1,
+        scoring=None,
+        cv=3,
+    ):
+        if isinstance(names,str):
+            names=[names]
+        assert isinstance(names,list)
+        for name in names:
+            assert name in self.y_df.columns.values
+
+        for name in names:
+            if (use_cols is None):
+                use_cols = self.x_df.drop(columns=self.key_fields).columns.values
+            self.col_dict[name] = use_cols
+            features = self.__get_values__(self.x_df,self.col_dict[name])
+            values   = self.__get_values__(self.y_df,name)
+
+            x_shuf, y_shuf = shuffle( features, values )
+            x_train, x_test, y_train, y_test = train_test_split( x_shuf, y_shuf, test_size=test_size )
+
+            if (parameters is None):
+                self.model_dict[name] = model.fit(x_train,y_train)
+            else:
+                gscv = GridSearchCV( model, parameters, n_jobs=n_jobs, scoring=scoring,cv=cv )
+                gscv.fit(x_train, y_train)
+                self.cv_dict[name] = gscv
+                self.model_dict[name] = gscv.best_estimator_.fit(x_train,y_train)
+
+            print("Fit model for "+name+", test data score=",str(self.model_dict[name].score(x_test,y_test)))
+
+    def predict(self,name,inp_df):
+        assert isinstance(name,str)
+        assert name in self.model_dict
+
+        features = self.__get_values__(inp_df,self.col_dict[name])
+        return self.model_dict[name].predict(features)
+
+    def predict_proba(self,name,inp_df):
+        assert isinstance(name,str)
+        assert name in self.model_dict
+
+        features = self.__get_values__(inp_df,self.col_dict[name])
+        return self.model_dict[name].predict_proba(features)
